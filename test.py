56e1bac457597bb2da59d4d77241d59cac4a3d02
Tad Leonard <tadfleonard@gmail.com>
2015-06-30 00:13:18 -0500

    and husl.py expect float RGB values

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 import husl_numpy as husl
+import husl as old_husl
 import imread
 
 
 def print_husl():
     img = imread.imread("examples/gelface.jpg")
     img_float = img / 255.0
-    print(husl.rgb_to_husl(img_float))
+    new = husl.rgb_to_husl(img_float)
+    old = old_husl.rgb_to_husl(*img_float[0, 0])
+    print(old, new[0, 0])
+    
 
 
 if __name__ == "__main__":
