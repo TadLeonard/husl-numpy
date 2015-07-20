46b33ed3443ba15c022fe37d3f80d7910ab7b531
Tad Leonard <tadfleonard@gmail.com>
2015-07-20 18:27:12 -0500

    added a HUSL 'rainbow' example, shuffled some things around

--- examples.py
+++ examples.py
@@ -11,8 +11,8 @@ def reveal_blue(img):
     # create a filter for pixels with hues between 50 and 100
     select = np.logical_and(hue > 250, hue < 290)
     out = img.copy()
-    out[select] = (0, 0, 255)  # reveal the selected region
-    return out
+    out[select] = 0xFF  # reveal the selected region
+    return out, "blue"
 
 
 def reveal_blue_rgb(img):
@@ -24,19 +24,38 @@ def reveal_blue_rgb(img):
     select = np.logical_and(B > R, B > G)  # no overpowering red or green
     select = np.logical_and(select, B > 125)  # strong enough blue channel
     out = img.copy()
-    out[select] = (0, 0, 255)  # reveal the selected region
-    return out
+    out[select] = 0xFF  # reveal the selected region
+    return out, "blue_rgb"
+
+
+def reveal_pink(img):
+    hue = nphusl.to_hue(img)  # a 2D array of HUSL hue values
+    pinkish = np.logical_or(hue < 10, hue > 320)  # orange pixel selection
+    out = img.copy()
+    out[pinkish] = 0xFF  # change selection to white
+    return out, "pink"
 
 
 def hue_graybow(img):
-    for hue in range(0, 400, 10)
+    out = img.copy()
+    hue = nphusl.to_hue(img)
+    for low, high in nphusl.chunk(360, 5):
+        is_odd = low % 10
+        select = np.logical_and(hue > low, hue < high)
+        c = int(2 * high / 3)
+        if is_odd:
+            color = c, 0x00, c
+        else:
+            color = 0x00, c, c // 2
+        out[select] = color
+    return out, "graybow"
 
 
 if __name__ == "__main__":
     filename = sys.argv[1]
     img = imread.imread(filename)
-    good = reveal_blue(img)
-    bad = reveal_blue_rgb(img) 
-    imread.imwrite("blue.jpg", good)
-    imread.imwrite("blue_bad.jpg", bad)
+    transforms = reveal_blue, reveal_blue_rgb, reveal_pink, hue_graybow
+    for t in transforms:
+        out, name = t(img)
+        imread.imwrite(name + ".jpg", out)
 
