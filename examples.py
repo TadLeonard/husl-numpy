263cc9881637cabf8eb58e25a49facc20939fd46
Tad Leonard <tadfleonard@gmail.com>
2015-07-20 16:33:24 -0500

    made blue hue highlighter example more informative with an RGB color space comparison

--- examples.py
+++ examples.py
@@ -4,19 +4,39 @@ import numpy as np
 import nphusl
 
 
-def reveal_red(img):
+def reveal_blue(img):
+    """Easy mode! Selecting bluish pixels with the HUSL color space."""
     # convert an integer RGB image to an 2D array of HUSL hue values
     hue = nphusl.to_hue(img)
     # create a filter for pixels with hues between 50 and 100
-    below_max = hue < 100
-    above_min = hue > 50
-    select = np.logical_and(below_max, above_min)
-    img[select] = (0, 200, 125)  # reveal the selected region
-    return img
+    select = np.logical_and(hue > 250, hue < 290)
+    out = img.copy()
+    out[select] = (0, 0, 255)  # reveal the selected region
+    return out
+
+
+def reveal_blue_rgb(img):
+    """Hard mode! Selecting blueish pixels without the convenience of the
+    HUSL color space."""
+    R, G, B = (img[..., n] for n in range(3))  # break out RGB color channels
+    # we'll try to create a "bluish" selection by choosing pixels for which
+    # the blue channel has a greater value than the others
+    select = np.logical_and(B > R, B > G)  # no overpowering red or green
+    select = np.logical_and(select, B > 125)  # strong enough blue channel
+    out = img.copy()
+    out[select] = (0, 0, 255)  # reveal the selected region
+    return out
+
+
+def hue_graybow(img):
+    for hue in range(0, 400, 10)
 
 
 if __name__ == "__main__":
     filename = sys.argv[1]
     img = imread.imread(filename)
-    reveal_red(img)
-    imread.imwrite("red.jpg", img)
+    good = reveal_blue(img)
+    bad = reveal_blue_rgb(img) 
+    imread.imwrite("blue.jpg", good)
+    imread.imwrite("blue_bad.jpg", bad)
+
