24c9422b88d964c81ff0ffff2652f34a7bc3cfaa
Tad Leonard <tadfleonard@gmail.com>
2015-07-20 20:12:03 -0500

    examples.py cleanup, clarifications

--- examples.py
+++ examples.py
@@ -9,9 +9,9 @@ def reveal_blue(img):
     # convert an integer RGB image to an 2D array of HUSL hue values
     hue = nphusl.to_hue(img)
     # create a filter for pixels with hues between 50 and 100
-    select = np.logical_and(hue > 250, hue < 290)
+    bluish = np.logical_and(hue > 250, hue < 290)
     out = img.copy()
-    out[select] = 0xFF  # reveal the selected region
+    out[~bluish] *= 0.5  # reveal the selected region
     return out, "blue"
 
 
@@ -21,40 +21,53 @@ def reveal_blue_rgb(img):
     R, G, B = (img[..., n] for n in range(3))  # break out RGB color channels
     # we'll try to create a "bluish" selection by choosing pixels for which
     # the blue channel has a greater value than the others
-    select = np.logical_and(B > R, B > G)  # no overpowering red or green
-    select = np.logical_and(select, B > 125)  # strong enough blue channel
+    bluish = np.logical_and(B > R, B > G)  # no overpowering red or green
+    bluish = np.logical_and(bluish, B > 125)  # strong enough blue channel
     out = img.copy()
-    out[select] = 0xFF  # reveal the selected region
+    out[~bluish] *= 0.5  # reveal the selected region
     return out, "blue_rgb"
 
 
-def reveal_pink(img):
-    hue = nphusl.to_hue(img)  # a 2D array of HUSL hue values
-    pinkish = np.logical_or(hue < 10, hue > 320)  # orange pixel selection
+def reveal_light(img):
+    hsl = nphusl.to_husl(img)  # a 2D array of HUSL hue values
+    lightness = hsl[..., 2]  # just the lightness channel
+    dark = lightness < 62
     out = img.copy()
-    out[pinkish] = 0xFF  # change selection to white
-    return out, "pink"
+    out[dark] = 0x00  # change selection to black
+    return out, "light"
 
 
-def hue_graybow(img):
+def reveal_light_rgb(img):
+    R, G, B = (img[..., n] for n in range(3))
+    luminance = 0.2126 * R + 0.7152 * G + 0.0722 * B
+    dark = luminance < 140
     out = img.copy()
-    hue = nphusl.to_hue(img)
-    for low, high in nphusl.chunk(360, 5):
-        is_odd = low % 10
+    out[dark] = 0x00  # change selection to black
+    return out, "light_rgb"
+
+
+def hue_watermelon(img):
+    out = img.copy()
+    hsl = nphusl.to_husl(img)
+    hue, _, lightness = (hsl[..., n] for n in range(3))
+    pink =  1.3, 0.3, 0.6 
+    green = 0.3, 1.1, 0.3
+    for low, high in nphusl.chunk(360, 5):  # chunks of the hue range
         select = np.logical_and(hue > low, hue < high)
-        c = int(2 * high / 3)
-        if is_odd:
-            color = c, 0x00, c
-        else:
-            color = 0x00, c, c // 2
-        out[select] = color
-    return out, "graybow"
+        is_odd = low % 10
+        color = pink if is_odd else green
+        out[select] *= color
+    for low, high in nphusl.chunk(100, 10):  # chunks of the lightness range
+        select = np.logical_and(lightness > low, lightness < high)
+        out[select] *= min(1, (high / 90.0))
+    return out, "watermelon"
 
 
 if __name__ == "__main__":
     filename = sys.argv[1]
     img = imread.imread(filename)
-    transforms = reveal_blue, reveal_blue_rgb, reveal_pink, hue_graybow
+    transforms = reveal_blue, reveal_blue_rgb, hue_watermelon,\
+                 reveal_light, reveal_light_rgb
     for t in transforms:
         out, name = t(img)
         imread.imwrite(name + ".jpg", out)
