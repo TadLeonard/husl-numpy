d34f1303abdd594381279329d645a66e008a632f
Tad Leonard <tadfleonard@gmail.com>
2015-08-15 16:27:23 -0500

    numexpr version of _to_linear is broken, working on it

--- _nphusl_expr.py
+++ _nphusl_expr.py
@@ -1,9 +1,13 @@
 import numpy as np
 from numpy import ndarray
 import numexpr as ne
+import husl
 
 
-@profile
+ne.set_num_threads(4)
+profile = lambda fn: fn
+
+
 def _to_linear(rgb_nd: ndarray) -> ndarray:
     a = 0.055  # mysterious constant used in husl.to_linear
     xyz_nd = np.zeros(rgb_nd.shape, dtype=np.float)
@@ -13,5 +17,19 @@ def _to_linear(rgb_nd: ndarray) -> ndarray:
     rgb_lt = rgb_nd[lt]
     xyz_nd[gt] = ne.evaluate("((rgb_gt + a) / (1 + a)) ** 2.4")
     xyz_nd[lt] = ne.evaluate("rgb_lt / 12.92")
+    #xyz_nd[gt] = ((rgb_gt + a) / (1 + a)) ** 2.4
+    #xyz_nd[lt] = rgb_lt / 12.92
     return xyz_nd
- 
+
+
+@profile
+def _f(y_nd: ndarray) -> ndarray:
+    y_flat = y_nd.flatten()
+    f_flat = np.zeros(y_flat.shape, dtype=np.float)
+    gt = y_flat > husl.epsilon
+    f_flat[gt] = (y_flat[gt] / husl.refY) ** (1.0 / 3.0) * 116 - 16
+    f_flat[~gt] = (y_flat[~gt] / husl.refY) * husl.kappa
+    return f_flat.reshape(y_nd.shape)
+
+
+
