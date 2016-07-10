2113a74fa39932bee2e1e85c44b60efef87beb32
Tad Leonard <tadfleonard@gmail.com>
2016-07-09 22:52:14 -0500

    compiling Cython extension with OpenMP

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from nphusl import __version__
 import numpy
 from Cython.Build import cythonize
 
 
+extensions = [
+    Extension("_nphusl_cython", sources=["_nphusl_cython.pyx"],
+              extra_compile_args=["-fopenmp"], extra_link_args=["-fopenmp"])
+]
+
+
 setup(name='nphusl',
       version=__version__,
       py_modules=['nphusl', "_nphusl_expr"],
-      ext_modules=cythonize("_nphusl_cython.pyx"),
+      #ext_modules=cythonize("_nphusl_cython.pyx"),
+      ext_modules=cythonize(extensions),
       include_dirs=[numpy.get_include()],
 )
+
+
