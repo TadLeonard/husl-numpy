877fa028cb7eafb0fe756dcd2346fdad916769a6
Tad Leonard <tadfleonard@gmail.com>
2016-07-13 01:19:17 -0500

    moved optimization selectors to __init__

---
+++ nphusl/__init__.py
@@ -0,0 +1,36 @@
+"""HUSL color space conversion.
+
+The basics:
+
+* to_rgb(hsl): Convert HUSL array to RGB integer array
+* to_husl(rgb): Convert RGB integer array or grayscale float array to HUSL array
+* to_hue(rgb): Convert RGB integer array or grayscale float array to array of hue values
+"""
+
+__version__ = "1.4.0"
+
+from . import nphusl as _nphusl
+from .nphusl import *
+from . import constants
+from . import _nphusl_expr
+from . import _nphusl_cython
+
+
+def enable_standard_fns():
+    for name, fn in STANDARD.items():
+        globals()[name] = fn
+        setattr(nphusl, name, fn)
+
+
+def enable_cython_fns():
+    for name, fn in CYTHON.items():
+        globals()[name] = fn
+        setattr(nphusl, name, fn)
+
+
+def enable_numexpr_fns():
+    for name, fn in NUMEXPR.items():
+        globals()[name] = fn
+        setattr(nphusl, name, fn)
+
+
