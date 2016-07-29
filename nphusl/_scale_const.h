722d0fd407e81a76413382114d3ef8731206dbc0
Tad Leonard <tadfleonard@gmail.com>
2016-07-29 04:08:49 -0400

    constants for max_chroma calc kept elsewhere
    also, discovered a nice optimization with the scalars of top1, top2, top2_b etc variables

---
+++ nphusl/_scale_const.h
@@ -0,0 +1,15 @@
+
+extern const double M[3][3];
+extern const double M_INV[3][3];
+extern const double REF_X;
+extern const double REF_Y;
+extern const double REF_Z;
+extern const double REF_U;
+extern const double REF_V;
+extern const double KAPPA;
+extern const double EPSILON;
+extern const double SCALE_SUB1[3];
+extern const double SCALE_BOTTOM[3];
+extern const double SCALE_SUB2;
+
+
