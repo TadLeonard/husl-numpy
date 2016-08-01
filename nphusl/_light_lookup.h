443d3c184206fac24a6e7de170e56abe1e18f738
Tad Leonard <tadfleonard@gmail.com>
2016-08-01 17:03:18 -0400

    generated light lookup tables may as well be checked in
    it would be annoying to have a LUT generation step as part of the build process

---
+++ nphusl/_light_lookup.h
@@ -0,0 +1,15 @@
+// nphusl/_light_lookup.h: generated with `python gen_light_table.py -y 0.070 0.350 -t float -o nphusl/_light_lookup -s 1024`
+
+
+extern const unsigned short L_TABLE_SIZE;
+extern const unsigned short L_BIG_TABLE_SIZE;
+typedef float l_table_t;
+extern const l_table_t y_thresh_0;
+extern const l_table_t y_thresh_1;
+extern const l_table_t light_table_0[1024];
+extern const l_table_t y_idx_step_0;
+extern const l_table_t light_table_1[1024];
+extern const l_table_t y_idx_step_1;
+extern const l_table_t light_table_2[1024];
+extern const l_table_t y_idx_step_2;
+extern const l_table_t big_light_table[3072];
