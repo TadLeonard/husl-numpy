f740de3244f77d351dbdcc4ea32056c37840bf33
Tad Leonard <tadfleonard@gmail.com>
2016-08-01 16:40:08 -0400

    luminance lookup table generator improved
    * selectable type (float/double) via command line
    * generates y-value stepsize variables for faster indexing of the table

--- gen_light_table.py
+++ gen_light_table.py
@@ -12,10 +12,17 @@ import nphusl
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--table-size", default=256, type=int)
 parser.add_argument("-o", "--output-file-prefix", default=None)
+parser.add_argument("-t", "--table-type", default="float",
+                    choices=["double", "float"])
+parser.add_argument("-y", "--y-steps", default=[0.05, 0.3],
+                    nargs="+", type=float)
+
 args = parser.parse_args()
 N = args.table_size
+table_type = args.table_type
 out = args.output_file_prefix
-
+y_steps = args.y_steps + [1.0]
+N_BIG = N * len(y_steps)
 
 """
 rgb = np.zeros(shape=(N, N, 3), dtype=np.uint8)
@@ -40,47 +47,71 @@ avg_spacing = np.sum(unique_light[1:] - unique_light[:-1]) / \
 print("Average light value spacing: {}".format(avg_spacing))
 """
 
+# set up file objects for .c and .h files
 out_h = open("{}.h".format(out), "w") if out else sys.stdout
 out_c = open("{}.c".format(out), "w") if out else sys.stdout
 out_header_name = os.path.split(out_h.name)[-1] if out else "<stdout>"
 
+# write "generated with" message at tops of files
 print("""// {}: generated with `python {}`
 
 """.format(out_h.name, " ".join(sys.argv), N), file=out_h)
-
 print("""// {}: generated with `python {}`
 
 #include <{}>
 
 """.format(out_c.name, " ".join(sys.argv), out_header_name), file=out_c)
 
-print("extern const int L_TABLE_SIZE;", file=out_h)
-print("const int L_TABLE_SIZE = {};".format(N), file=out_c)
+# declare table types, sizes
+print("extern const unsigned short L_TABLE_SIZE;", file=out_h)
+print("extern const unsigned short L_BIG_TABLE_SIZE;".format(N_BIG), file=out_h)
+print("const unsigned short L_TABLE_SIZE = {};".format(N), file=out_c)
+print("const unsigned short L_BIG_TABLE_SIZE = {};".format(N_BIG), file=out_c)
+print("typedef {} l_table_t;".format(table_type), file=out_h)
 
-steps = (0.05, 0.3, 1.0)
-for i, step in enumerate(steps[:-1]):
-    print("extern const float light_step_{};".format(i, step), file=out_h)
-    print("extern const float light_table_{}[{}];".format(i, N), file=out_h)
-    print("const float light_step_{} = {:0.04f};".format(i, step), file=out_c)
-print("extern const float light_table_{}[{}];".format(len(steps)-1, N), file=out_h)
-print("", file=out_c)
+# declare tables, Y value steps
+for i, step in enumerate(y_steps[:-1]):
+    print("extern const l_table_t y_thresh_{};".format(i), file=out_h)
 start = 0.0
-for i, stop in enumerate(steps):
+for i, step in enumerate(y_steps):
+    y_idx_step = (step - start) / N
+    print("extern const l_table_t light_table_{}[{}];".format(i, N), file=out_h)
+    print("extern const l_table_t y_idx_step_{};".format(i), file=out_h)
+    print("const l_table_t y_idx_step_{} = {};".format(i, y_idx_step), file=out_c)
+    print("const l_table_t y_thresh_{} = {:0.04f};".format(i, step), file=out_c)
+    start = step
+print("extern const l_table_t big_light_table[{}];".format(N_BIG), file=out_h)
+print("", file=out_c)
+big_light_lookup = np.zeros((N_BIG,), dtype=float)
+
+# write out little tables
+start = 0.0
+for i, stop in enumerate(y_steps):
     # Generate our LUT from a range of Y-values in [0, 1.0)
     # NOTE: this uniform range works because L increases monitonically with y
     step = (stop - start) / N
     uniform_y = np.arange(start, stop, step=step)
-    light_lookup_table = nphusl._nphusl._f(uniform_y)
-    avg_light_step = \
-        np.sum(light_lookup_table[1:] - light_lookup_table[:-1]) / (N-1)
+    light_lookup = nphusl._nphusl._f(uniform_y)
+    big_light_lookup[i*N: i*N+N] = light_lookup
+    avg_y_thresh = \
+        np.sum(light_lookup[1:] - light_lookup[:-1]) / (N-1)
     print("// Avg light value step size: {:0.4f}".format(
-        avg_light_step), file=out_c)
+        avg_y_thresh), file=out_c)
     print("// Light values for {} <= Y < {}".format(start, stop), file=out_c)
-    print("const float light_table_{}[{}] = {{".format(
-        i, N), file=out_c)
-    for i, l in enumerate(light_lookup_table):
+    print("const l_table_t light_table_{}[{}] = {{".format(
+          i, N), file=out_c)
+    for i, l in enumerate(light_lookup):
         print("{:6.3f}".format(l), end=", ", file=out_c)
         if not i % 8 and i:
             print("", file=out_c)
     print("};\n", file=out_c)
     start = stop
+
+# write out big table (all values concatenated into one)
+print("const l_table_t big_light_table[{}] = {{".format(
+      N_BIG), file=out_c)
+for i, l in enumerate(big_light_lookup):
+    print("{:6.3f}".format(l), end=", ", file=out_c)
+    if not i % 8 and i:
+        print("", file=out_c)
+print("};\n", file=out_c)
