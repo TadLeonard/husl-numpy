d1b98a97e41b7317b5837883085fee700e6b3122
Tad Leonard <tadfleonard@gmail.com>
2016-08-02 21:51:50 -0400

    SIMD Cython wrapper simplified

--- nphusl/_simd_opt.pyx
+++ nphusl/_simd_opt.pyx
@@ -4,33 +4,30 @@ import cython
 
 
 cdef extern from "_simd.h":
-    double *rgb_to_husl_nd(double*, int, int)
+    ctypedef double double
+    double *rgb_to_husl_nd(double*, int, int, int)
 
 
 def rgb_to_husl(rgb):
+    cdef int rows = rgb.shape[0]
+    cdef int cols = rgb.shape[1]
+    cdef int flat = len(rgb.shape) < 3
     cdef double[::1] hsl_flat
-    if len(rgb.shape) == 3:
-        hsl_flat = rgb_to_husl_3d(rgb)
+    if flat:
+        hsl_flat = _rgb_to_husl_2d(rgb, rows, cols)
     else:
-        hsl_flat = rgb_to_husl_2d(rgb)
+        hsl_flat = _rgb_to_husl_3d(rgb, rows, cols)
     return np.asarray(hsl_flat).reshape(rgb.shape)
 
 
-cdef double[::1] rgb_to_husl_3d(double[:, :, ::1] rgb):
-    cdef int rows = rgb.shape[0]
-    cdef int cols = rgb.shape[1]
-
-    cdef double *rgb_ptr = &rgb[0, 0, 0]
-    cdef double *hsl_ptr = rgb_to_husl_nd(rgb_ptr, rows, cols)
+cdef double[::1] _rgb_to_husl_3d(double[:, :, ::1] rgb, int rows, int cols):
+    cdef double *hsl_ptr = rgb_to_husl_nd(&rgb[0, 0, 0], rows, cols, 0)
     cdef double[::1] husl = <double[:rgb.size]> hsl_ptr
     return husl
 
 
-cdef double[::1] rgb_to_husl_2d(double[:, ::1] rgb):
-    cdef int rows = rgb.shape[0]
-    cdef int cols = rgb.shape[1]
-
-    cdef double *rgb_ptr = &rgb[0, 0]
-    cdef double *hsl_ptr = rgb_to_husl_nd(rgb_ptr, rows, cols)
+cdef double[::1] _rgb_to_husl_2d(double[:, ::1] rgb, int rows, int cols):
+    cdef double *hsl_ptr = rgb_to_husl_nd(&rgb[0, 0], rows, cols, 1)
     cdef double[::1] husl = <double[:rgb.size]> hsl_ptr
     return husl
+
