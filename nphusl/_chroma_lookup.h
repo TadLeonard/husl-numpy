f768fa183a72da4fcd6ad27848736d8bd6a95afa
Tad Leonard <tadfleonard@gmail.com>
2016-08-02 22:05:47 -0400

    chroma lookup tables
    as with the luminance LUTs, it's less of a hassle to just commit and include the generated LUTs
    instead of including their generation in the build script

---
+++ nphusl/_chroma_lookup.h
@@ -0,0 +1,8 @@
+// nphusl/_chroma_lookup.h: generated with `python gen_chroma_table.py -t float -o nphusl/_chroma_lookup`
+
+
+extern const unsigned short C_TABLE_SIZE;
+typedef float c_table_t;
+extern const c_table_t chroma_table[256][256];
+extern const c_table_t h_idx_step_255;
+extern const c_table_t l_idx_step_255;
