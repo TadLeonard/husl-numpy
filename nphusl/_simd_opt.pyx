3e39866212578c77248f66a69a32df64958e3554
Tad Leonard <tadfleonard@gmail.com>
2016-08-07 13:50:52 -0400

    simplified Cython wrapper of C impl
    the Cython wrapper now flattens the RGB data, so a 3D wrapper fn is no longer necessary

--- nphusl/_simd_opt.pyx
+++ nphusl/_simd_opt.pyx
@@ -7,34 +7,27 @@ import cython
 from . import transform
 
 
-data_type = np.float
-ctypedef np.float_t data_type_t
+hsl_type = np.float64
+ctypedef np.float64_t hsl_t
+cdef size_t data_size = sizeof(hsl_t)
 
 
 cdef extern from "_simd.h":
-    data_type_t* rgb_to_husl_nd(np.uint8_t *rgb, int size)
+    hsl_t* rgb_to_husl_nd(np.uint8_t *rgb, int size)
 
 
 @transform.rgb_int_input
 def _rgb_to_husl(rgb):
     cdef int size = rgb.size
-    cdef int flat = len(rgb.shape) < 3
-    cdef data_type_t[::1] hsl_flat
-    if flat:
-        hsl_flat = _rgb_to_husl_2d(rgb, size)
-    else:
-        hsl_flat = _rgb_to_husl_3d(rgb, size)
-    return np.asarray(hsl_flat).reshape(rgb.shape)
+    cdef int pixels = size / 3
+    rgb_flat = rgb.reshape((pixels, 3))
+    cdef hsl_t[::1] hsl_flat
+    hsl_flat = _rgb_to_husl_2d(rgb_flat, size)
+    return np.asarray(hsl_flat, dtype=hsl_type).reshape(rgb.shape)
 
 
-cdef data_type_t[::1] _rgb_to_husl_3d(np.uint8_t[:, :, ::1] rgb, int size):
-    cdef data_type_t *hsl_ptr = rgb_to_husl_nd(&rgb[0, 0, 0], size)
-    cdef data_type_t[::1] husl = <data_type_t[:size]> hsl_ptr
-    return husl
-
-
-cdef data_type_t[::1] _rgb_to_husl_2d(np.uint8_t[:, ::1] rgb, int size):
-    cdef data_type_t *hsl_ptr = rgb_to_husl_nd(&rgb[0, 0], size)
-    cdef data_type_t[::1] husl = <data_type_t[:size]> hsl_ptr
+cdef hsl_t[::1] _rgb_to_husl_2d(np.uint8_t[:, ::1] rgb, int size):
+    cdef hsl_t *hsl_ptr = rgb_to_husl_nd(&rgb[0, 0], size)
+    cdef hsl_t[::1] husl = <hsl_t[:size]> hsl_ptr
     return husl
 
