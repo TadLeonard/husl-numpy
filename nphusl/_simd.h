1e24c4cee88c42ae1e6c1e6bdba4f2490bfb70cb
Tad Leonard <tadfleonard@gmail.com>
2016-08-09 06:28:01 -0400

    added rgb_to_husl_triplet for computing HUSL values from a single RGB triplet

--- nphusl/_simd.h
+++ nphusl/_simd.h
@@ -1,5 +1,7 @@
 
-typedef unsigned char uint8;
+#include <stdint.h>
+
 typedef double hsl_type;
-extern hsl_type *rgb_to_husl_nd(uint8* rgb, int size);
+extern hsl_type *rgb_to_husl_nd(uint8_t* rgb, size_t size);
+extern hsl_type *rgb_to_husl_triplet(uint8_t r, uint8_t g, uint8_t b);
 
