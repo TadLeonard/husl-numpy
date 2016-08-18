02e51fa742059b6055591993ef69ed5cf28098b3
Tad Leonard <tadfleonard@gmail.com>
2016-08-18 04:10:17 -0400

    in testing, I found that a smaller luminance LUT works
    we get a max error of < 0.1 for luminance in the interval [0.0, 100.0], so that's absolutely good enough. Smaller tables give a very mild speedup, probably because they're nicer to the cache.

--- nphusl/_light_lookup.h
+++ nphusl/_light_lookup.h
@@ -1,17 +1,15 @@
-// nphusl/_light_lookup.h: generated with `python lookup_generators/gen_light_table.py -y 0.070 0.350 -t double -o nphusl/_light_lookup -s 1024`
+// nphusl/_light_lookup.h: generated with `python lookup_generators/gen_light_table.py -y 0.070 0.350 -t double -o nphusl/_light_lookup -s 256`
 
 
-extern const unsigned short L_SEGMENT_SIZE;
-extern const unsigned short L_FULL_TABLE_SIZE;
+const unsigned short L_SEGMENT_SIZE;
+const unsigned short L_FULL_TABLE_SIZE;
 typedef double l_table_t;
-extern const l_table_t y_thresh_0;
-extern const l_table_t y_thresh_1;
-extern const l_table_t light_table_0[1024];
-extern const l_table_t y_idx_step_0;
-extern const l_table_t light_table_1[1024];
-extern const l_table_t y_idx_step_1;
-extern const l_table_t light_table_2[1024];
-extern const l_table_t y_idx_step_2;
-extern const l_table_t light_table_linear[1024];
-extern const l_table_t y_idx_step_linear;
-extern const l_table_t light_table_big[3072];
+const l_table_t y_thresh_0;
+const l_table_t y_thresh_1;
+const l_table_t light_table_0[256];
+const l_table_t y_idx_step_0;
+const l_table_t light_table_1[256];
+const l_table_t y_idx_step_1;
+const l_table_t light_table_2[256];
+const l_table_t y_idx_step_2;
+const l_table_t light_table_big[768];
