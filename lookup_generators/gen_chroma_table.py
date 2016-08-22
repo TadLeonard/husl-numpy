830079accdb3fc7f895f9abcf3df38ea4656d61e
Tad Leonard <tadfleonard@gmail.com>
2016-08-22 19:17:42 -0400

    chroma LUT size for each dimension can be specified
    instead of having a square (NxN) chroma LUT, the LUT generator script lets you
    choose the size of each dimension. Since HUSL has such sharp chroma peaks in the luminance-hue space,
    the ability to play with the size of each dimension is helpful

--- lookup_generators/gen_chroma_table.py
+++ lookup_generators/gen_chroma_table.py
@@ -14,13 +14,15 @@ import alignment
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--table-size", default=256, type=int)
+parser.add_argument("-u",  "--hue-axis-size", default=256, type=int)
+parser.add_argument("-l", "--light-axis-size", default=256, type=int)
 parser.add_argument("-o", "--output-file-prefix", default=None)
 parser.add_argument("-t", "--table-type", default="float",
                     choices=["double", "float"])
 
 args = parser.parse_args()
-N = args.table_size
+NL = args.light_axis_size
+NH = args.hue_axis_size
 table_type = args.table_type
 out = args.output_file_prefix
 
@@ -32,7 +34,7 @@ out_header_name = os.path.split(out_h.name)[-1] if out else "<stdout>"
 # write "generated with" message at tops of files
 print("""// {}: generated with `python {}`
 
-""".format(out_h.name, " ".join(sys.argv), N), file=out_h)
+""".format(out_h.name, " ".join(sys.argv)), file=out_h)
 print("""// {}: generated with `python {}`
 
 #include <{}>
@@ -40,49 +42,52 @@ print("""// {}: generated with `python {}`
 """.format(out_c.name, " ".join(sys.argv), out_header_name), file=out_c)
 
 # declare table types, sizes
-print("const unsigned short C_TABLE_SIZE;", file=out_h)
-print("const unsigned short C_TABLE_SIZE = {};".format(N), file=out_c)
+print("const unsigned short CH_TABLE_SIZE;", file=out_h)
+print("const unsigned short CL_TABLE_SIZE;", file=out_h)
+print("const unsigned short CH_TABLE_SIZE = {};".format(NH), file=out_c)
+print("const unsigned short CL_TABLE_SIZE = {};".format(NL), file=out_c)
 print("typedef {} c_table_t;".format(table_type), file=out_h)
 
 # build chroma table
-h_idx_step = 360.0 / (N-1)
-l_idx_step = 100.0 / (N-1)
+h_idx_step = 360.0 / (NH-1)
+l_idx_step = 100.0 / (NL-1)
 hues = np.arange(0, 360+h_idx_step, step=h_idx_step)
 lights = np.arange(0, 100+l_idx_step, step=l_idx_step)
-chroma_table = np.zeros(shape=(N, N), dtype=table_type)
+chroma_table = np.zeros(shape=(NH, NL), dtype=table_type)
 for i, hue in enumerate(hues):
     for j, light in enumerate(lights):
         if light != 0:  # NaN @ light=0, where chroma is always 0
             chroma_table[i][j] = husl.max_chroma_for_LH(light, hue)
-#chroma_table = np.nan_to_num(chroma_table)  # NaN @ hue=0
 
 # declare table, consts
-print("const c_table_t chroma_table[{}][{}];".format(N, N), file=out_h)
+print("const c_table_t chroma_table[{}][{}];".format(NH, NL), file=out_h)
 print("const c_table_t h_idx_step;", file=out_h)
 print("const c_table_t l_idx_step;", file=out_h)
 print("const c_table_t h_idx_step = {};".format(h_idx_step), file=out_c)
 print("const c_table_t l_idx_step = {};".format(l_idx_step), file=out_c)
 
 # some statistics on the lookup tables to gauge their accuracy
-h_diff = np.sum(np.abs(chroma_table[1:, :] - chroma_table[:-1, :]), axis=1) / (N-1)
+h_diff = np.abs(chroma_table[1:, :] - chroma_table[:-1, :])
 h_max = np.max(h_diff)
-h_diff = np.sum(h_diff) / len(h_diff)
-l_diff = np.sum(np.abs(chroma_table[:, 1:] - chroma_table[:, :-1]), axis=0) / (N-1)
+h_avg = np.sum(h_diff) / ((NH-1)*NL)
+l_diff = np.abs(chroma_table[:, 1:] - chroma_table[:, :-1])
 l_max = np.max(l_diff)
-l_diff = np.sum(l_diff) / len(l_diff)
+l_avg = np.sum(l_diff) / (NH*(NL-1))
 print("", file=out_c)
-print("// Ave delta across hue (1st axis): {}".format(h_diff),  file=out_c)
+print("// Ave delta across hue (1st axis): {}".format(h_avg),  file=out_c)
 print("// Max delta across hue: {}".format(h_max), file=out_c)
-print("// Ave delta across luminance (2nd axis): {}".format(l_diff), file=out_c)
+print("// Ave delta across luminance (2nd axis): {}".format(l_avg), file=out_c)
 print("// Max delta across luminance: {}".format(l_max), file=out_c)
 
 # write out table initializer
 alignment = "__attribute__((aligned({})))".format(alignment.N)
 print("const c_table_t {} chroma_table[{}][{}] = {{".format(
-      alignment, N, N), file=out_c)
+      alignment, NH, NL), file=out_c)
 for i, hue in enumerate(hues):
+    max_delta = np.max(np.abs(chroma_table[i, 1:] - chroma_table[i, :-1]))
     print("  // Index {}: Chromas for hue={} and luminance in [0, 100]"
           .format(i, hue), file=out_c)
+    print("  // Max chroma delta: {}".format(max_delta), file=out_c)
     print("  {", file=out_c)
     print("    ", end="", file=out_c)
     l_start = 0
