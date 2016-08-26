c66b0016724ccd692af88d856cb1a344ad112983
Tad Leonard <tadfleonard@gmail.com>
2016-08-26 03:44:19 -0400

    Removed generated LUTs from source
    It isn't as convenient to keep these in the source as I thought it'd be.
    They're changing too often in testing. Also, it's proven useful to generate
    enormous (1024*1024) tables instead of doing expensive bilinear interpolations.

--- nphusl/_chroma_lookup.h
+++
@@ -1,8 +0,0 @@
-// nphusl/_chroma_lookup.h: generated with `python lookup_generators/gen_chroma_table.py -t double -o nphusl/_chroma_lookup -s 256`
-
-
-extern const unsigned short C_TABLE_SIZE;
-typedef double c_table_t;
-const c_table_t chroma_table[256][256];
-extern const c_table_t h_idx_step;
-extern const c_table_t l_idx_step;
